98c1d6bed42f841d818cf2d00a08ca31cc5a2114
simyebin1 <syb9134@sookmyung.ac.kr>
2026-07-28 23:05:17 +0900

    Clean up app.py by removing empty write calls
    Removed empty write statements and cleaned up code.

--- app.py
+++ app.py
@@ -1,11 +1,11 @@
 import streamlit as st
 
 st.title("오늘의 말차")
-
+st.write("")
 st.write("오늘의 말차를 만들어 보세요.")
-
-
-
-
-
+st.write("")
+st.write("")
+st.write("")
+st.write("")
+st.write("")
 st.button("시작하기")
